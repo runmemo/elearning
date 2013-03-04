9be684e6f106ac299fdaed6acbeba6287940f2b6
quickstart <quickstart@qs10>
2013-03-04 17:52:16 -0600

    Book Outline custom module added.

---
+++ sites/all/modules/custom/bookoblock/bookoblock.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+jQuery('.block-bookoblock .menu').accordion({ 
+    header: 'div.title', 
+    active: false, 
+    alwaysOpen: false, 
+    animated: false, 
+    autoHeight: false 
+});
