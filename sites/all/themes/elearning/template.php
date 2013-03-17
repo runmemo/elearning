5e9c539ff926715bae122b5f24bacb6e3c5ea544
quickstart <quickstart@qs10>
2013-03-16 20:39:04 -0500

    Refactored course outline to use renderable array. Added custom theming for theme_menu_links to add level classes to book outline list items.

--- sites/all/themes/elearning/template.php
+++ sites/all/themes/elearning/template.php
@@ -10,3 +10,21 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+function elearning_menu_link(array $variables) {
+  //print_nice($variables);
+  $element = $variables['element'];
+  $depth = $element['#original_link']['depth'];
+  $sub_menu = '';
+  if (isset($element['#original_link']['module']) 
+        && $element['#original_link']['module'] == 'book') {
+    $element['#attributes']['class'][] = 'level-' . $depth;
+  }
+    
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }  
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
