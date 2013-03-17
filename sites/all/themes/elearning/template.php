c4e715595b556f6031935b1da1333221c6ba676c
quickstart <quickstart@qs10>
2013-03-17 18:41:55 -0500

    Moved course outline theming to module.

--- sites/all/themes/elearning/template.php
+++ sites/all/themes/elearning/template.php
@@ -10,23 +10,3 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
-
-function elearning_menu_link(array $variables) {
-  $element = $variables['element'];
-  $depth = $element['#original_link']['depth'];
-  $sub_menu = '';
-  $is_book_menu = ($element['#original_link']['module'] === 'book');
-  
-  if ($is_book_menu && $depth == 2) {
-    $output = '<h3>' . $element['#title'] . '</h3>';
-  } 
-  else { // not unit of the book
-    $link  = l($element['#title'], $element['#href'], $element['#localized_options']);
-    $output =  $link;
-  }
-    
-  if ($element['#below']) {
-    $sub_menu = drupal_render($element['#below']);
-  }  
-  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
-}
