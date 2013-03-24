0c3f0c9d37092277b45a86764c6ae47a8c0ba032
Bulat <byapparov@gmail.com>
2013-03-24 12:24:56 +0000

    Fixed bug with multiple binding of click event in course outline.

--- sites/all/modules/custom/course_outline/course_outline.js
+++ sites/all/modules/custom/course_outline/course_outline.js
@@ -5,9 +5,9 @@
 (function ($) {
   Drupal.behaviors.course_outline_accordion = {    
     attach: function(context, settings) {
-      $('.block-course-outline h3').click(function () {
-        $(this).next('ul').slideToggle();
-      });
+        $('.block-course-outline h3').unbind('click').bind('click', function () {
+          $(this).next('ul').slideToggle();
+        });
 	// open section with active page
         $('.block-course-outline .menu > ul').hide();
 	$('.block-course-outline .active').closest('ul').show();    
