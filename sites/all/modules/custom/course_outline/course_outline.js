1423636a3df6ace926e4091ce384c1a2914a15fd
Bulat <byapparov@gmail.com>
2013-03-24 11:59:19 +0000

    Changed the outline js - removed too many accordion classes not needed.

--- sites/all/modules/custom/course_outline/course_outline.js
+++ sites/all/modules/custom/course_outline/course_outline.js
@@ -3,34 +3,13 @@
  */
 
 (function ($) {
-    // custom accordion toggle that allows several sections to be open
-    // see: http://jsfiddle.net/DkHyd/
-    $.fn.togglepanels = function(){
-	return this.each(function(){
-	  $(this).addClass("ui-accordion ui-accordion-icons ui-widget ui-helper-reset")
-	  .find('h3')
-	  .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
-	  .hover(function() { $(this).toggleClass("ui-state-hover"); })
-	  .prepend('<span class="ui-icon ui-icon-triangle-1-e"></span>')
-	  .click(function() {
-	    $(this)
-	      .toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom")
-	      .find("> .ui-icon").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s").end()
-	      .next().slideToggle();
-	    return false;
-	  })
-	  .next()
-	    .addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom")
-	    .hide();
-	});
-      };  
-    
-  Drupal.behaviors.course_outline_accordion = {
+  Drupal.behaviors.course_outline_accordion = {    
     attach: function(context, settings) {
-	// add accordion to course outline
-	$('.block-course-outline').togglepanels();
-	// open section with active page
-	$('.ui-accordion-content .active').closest('ul').prev().click();    
+      $('.block-course-outline h3').click(function () {
+        $(this).next('ul').slideToggle();
+      });
+	// @todo: open section with active page
+	//$('. .active').closest('ul').prev().click();    
     }
   };
 })(jQuery);
