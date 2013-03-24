33ef62407fb37db93001eba25d3c1ddd08e39af7
Barinder Singh <sainibarinder@gmail.com>
2013-03-24 19:45:35 +0530

    Lesson Hovers

--- sites/all/modules/custom/lesson_questions/lesson_questions.js
+++ sites/all/modules/custom/lesson_questions/lesson_questions.js
@@ -4,13 +4,6 @@
       $('span.close').click(function() {
         $(this).parent().hide('fast');
       });
-      $('.view-relevant-questions-list .views-field-field-answer-count .field-content').each(function() {
-        if($(this).text() <= 0) {
-          $(this).parent().parent().addClass('zero');
-        }
-      });
-
-        $('li a.active').parent().addClass('active');
     }
   };
 })(jQuery);
