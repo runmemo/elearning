f7377aca7a595529c784963a60a5bbd460b891e7
Barinder Singh <sainibarinder@gmail.com>
2013-03-26 01:16:54 +0530

    Lesson Page changes

--- sites/all/modules/custom/course_outline/course_outline.js
+++ sites/all/modules/custom/course_outline/course_outline.js
@@ -5,12 +5,18 @@
 (function ($) {
   Drupal.behaviors.course_outline_accordion = {    
     attach: function(context, settings) {
+        $('.block-course-outline h3').last().addClass('last');
+        $('.block-course-outline a.active').parent().parent().prev().addClass('active');
         $('.block-course-outline h3').unbind('click').bind('click', function () {
           $(this).next('ul').slideToggle();
+          $(this).toggleClass('active');
         });
 	// open section with active page
         $('.block-course-outline .menu > ul').hide();
-	$('.block-course-outline .active').closest('ul').show();    
+	$('.block-course-outline .active').closest('ul').show();
+
+        $('.book-navigation a.page-up').html($('.block-course-outline a.active').html());
+        $('.book-navigation a.page-up').attr('href', '#');   
     }
   };
 })(jQuery);
