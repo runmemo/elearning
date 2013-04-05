3a444e2faa173a59ae5ed478e8b45893cfb2021d
Ilya Novikov <ilya.no@developmentmill.com>
2013-04-05 21:12:58 +0700

    update for class progress bar

--- sites/all/modules/custom/course_outline/course_outline.js
+++ sites/all/modules/custom/course_outline/course_outline.js
@@ -2,21 +2,29 @@
  * @file conteins js for course outline navigation
  */
 
-(function ($) {
-  Drupal.behaviors.course_outline_accordion = {    
+(function($) {
+  Drupal.behaviors.course_outline_accordion = {
     attach: function(context, settings) {
-        $('.block-course-outline h3').last().addClass('last');
-        $('.block-course-outline a.active').parent().parent().prev().addClass('active');
-        $('.block-course-outline h3').unbind('click').bind('click', function () {
-          $(this).next('ul').slideToggle();
-          $(this).toggleClass('active');
+      $('.block-course-outline h3').last().addClass('last');
+      // add background to a instead of li, because of progress-bar
+      $('.block-course-outline li').hover(
+        function() {
+        $(this).find('a').css('background', '#fff0d9');
+      },
+        function() {
+          $(this).find('a').css('background', '#ffffff');
         });
-	// open section with active page
-        $('.block-course-outline .menu > ul').hide();
-	$('.block-course-outline .active').closest('ul').show();
+      $('.block-course-outline a.active').parent().parent().prev().addClass('active');
+      $('.block-course-outline h3').unbind('click').bind('click', function() {
+        $(this).next('ul').slideToggle().css('display', 'inline-block');
+        $(this).toggleClass('active');
+      });
+      // open section with active page
+      $('.block-course-outline .menu > ul').css('display', 'none');
+      $('.block-course-outline .active').closest('ul').css('display', 'inline-block');
 
-        $('.book-navigation a.page-up').html($('.block-course-outline a.active').html());
-        $('.book-navigation a.page-up').attr('href', '#');   
+      $('.book-navigation a.page-up').html($('.block-course-outline a.active').html());
+      $('.book-navigation a.page-up').attr('href', '#');
     }
   };
 })(jQuery);
