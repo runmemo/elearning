67c71a09ca57f8391e3d2045a12c58709f4db613
Bulat <byapparov@gmail.com>
2013-04-13 23:04:20 +0100

    Fixed error in book-header.tpl.php about $provider_logo variable missing.

--- sites/all/modules/custom/course/book-header.tpl.php
+++ sites/all/modules/custom/course/book-header.tpl.php
@@ -1,22 +1,18 @@
+<?php
+// @todo barinder
+// provide comments for the variables available in this tpl.php file.
+?>
 <div class="book-header">
-  <?php if ($provider_logo): ?>
     <div class="provider-logo">
       <?php print $provider_logo; ?>
-    </div>
-  <?php endif; ?>
-
-  <div class="title-wrapper">
-    <?php if ($course_title): ?>
+    </div>  
+   <div class="title-wrapper">
       <div class="course-title">
         <?php print $course_title; ?>
       </div>
-    <?php endif; ?>
-
-    <?php if ($teacher_name || $teacher_surname): ?>
       <div class="teacher-name">
         <?php print $teacher_name . '&nbsp;' . $teacher_surname; ?>
       </div>
-    <?php endif; ?>
   </div>
-  <?php if (isset($lesson_link)) print $lesson_link; ?>
+  <?php print $lesson_link; ?>
 </div>
