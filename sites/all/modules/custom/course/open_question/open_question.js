b7d91b28282591f42bfc931edca73a9672be014e
Ilya Novikov <ilya.no@developmentmill.com>
2013-04-29 19:42:30 +0700

    update for oq feedbacks comments, new custom module has been created

--- sites/all/modules/custom/course/open_question/open_question.js
+++ sites/all/modules/custom/course/open_question/open_question.js
@@ -1,31 +1,8 @@
 /* 
- * @file conteins js for best answer selection process
+ * @file contents js for fedback process on open question review list.
  */
 
 (function($) {
-  // @todo ilya move to inline_comments_extra
-  Drupal.behaviors.open_question_inline_comments = {
-    attach: function(context, settings) {
-      // inline comments behavior
-      $('a.comment-link').mousedown(function() {
-        $(this).click();
-        $(this).parent().addClass('hidden');
-        // insert comment form after list of comments
-        var comments = $(this).parents('.views-row').find('.comment-group');
-        $(this).parents('.views-row').find('.comment-form').insertAfter(comments);
-      });
-      $('.comment-form').find('.form-submit').click(function() {
-        $(this).parents('.views-row').find('.views-inline-comment-field').removeClass('hidden').find('.comment-link').click();
-      });
-      // hide comments form
-      $('.comment-cancel').click(function() {
-        var nid = parseInt($(this).attr('id').replace('comment-form-hide-',''), 10);
-        $('#comment-form-show-' + nid).removeClass('hidden');
-        $(this).parents('.views-row').find('.formcloselink').trigger('click');
-      });
-    }
-  };
-  
   Drupal.behaviors.open_question_review_list = {
     attach: function(context, settings) {
        // feedback form behavior
