f75d9568698cfd20ad32b849e11d73864aeee77a
Ilya Novikov <ilya.no@developmentmill.com>
2013-05-02 20:58:04 +0700

    fix for 'Do not add' button and tmp solution for low rating submit

--- sites/all/modules/custom/course/open_question/open_question.js
+++ sites/all/modules/custom/course/open_question/open_question.js
@@ -1,5 +1,5 @@
 /* 
- * @file contents js for fedback process on open question review list.
+ * @file contents js for feedback process on open question review list.
  */
 
 (function($) {
@@ -20,34 +20,42 @@
         var review = $(this).parents('.views-field-feedback-form').find('.form-item-inactive');
         var submit = $(this).parents('.views-field-feedback-form').find('.oq-review-form-submit-button');
         var submit_wo_text = $(this).parents('.views-field-feedback-form').find('.oq-review-form-submit-wo-text');
+        var textarea = $(this).parents('.views-field-feedback-form').find('textarea[name="review"]').val();
         review.removeClass('.form-item-inactive').addClass('form-item-active');
         submit.addClass('form-item-active');
-        if (this.selectedIndex <= 3) {
-          var textarea = $(this).parents('.views-field-feedback-form').find('textarea[name="review"]');
-          if (textarea.val() === '') {
+        submit_wo_text.attr('disabled', 'disabled').removeClass('form-item-active');
+        if (this.selectedIndex <= 3) {  
+          if (textarea === '') {
             submit.attr('disabled', 'disabled').addClass('disabled');
-            submit_wo_text.addClass('form-item-active');
-            submit_wo_text.removeAttr('disabled');
           }
         } else {
+          if (textarea === '') {
+            submit_wo_text.removeAttr('disabled').addClass('form-item-active');
+          } else {
+            submit_wo_text.attr('disabled', 'disabled').removeClass('form-item-active');
+          }
           submit.removeClass('disabled').removeAttr('disabled');
-          submit_wo_text.attr('disabled', 'disabled').removeClass('form-item-active');
         }
         var rating_submit = $(this).parents('.views-field-feedback-form').find('.oq-review-form-rating-submit');
         if (rating_submit.length > 0) {
           rating_submit.trigger('mousedown');
         }
       });
-
       $('textarea[name="review"]').keyup(function() {
         var select = $(this).parents('.views-field-feedback-form').find('select[name="rating"]');
-        if ($(this).val().length < 4 && select[0].selectedIndex <= 3) {
+        var rating = select[0].selectedIndex;
+        var text = $(this).val().length;
+        if (text < 1 && rating > 3) {
+          $(this).parents('.views-field-feedback-form').find('.oq-review-form-submit-wo-text').addClass('form-item-active').removeAttr('disabled');
+        } else {
+          if (text >= 1) {
+            $(this).parents('.views-field-feedback-form').find('.oq-review-form-submit-wo-text').removeClass('form-item-active').attr('disabled', 'disabled');
+          }
+        }
+        if (text < 4 && rating <= 3) {
           $(this).parents('.views-field-feedback-form').find('.oq-review-form-submit-button').attr('disabled', 'disabled').addClass('disabled');
         } else {
-          if ($(this).val().length === 0) {
-            $(this).parents('.views-field-feedback-form').find('.oq-review-form-submit-wo-text').removeClass('disabled').removeAttr('disabled');
-          }
-          if (($(this).val().length >= 4 && select[0].selectedIndex <= 3) || (select.selectedIndex > 3)) {
+          if ((text >= 4 && rating <= 3) || (rating > 3)) {
             $(this).parents('.views-field-feedback-form').find('.oq-review-form-submit-button').removeClass('disabled').removeAttr('disabled');
           }
         }
