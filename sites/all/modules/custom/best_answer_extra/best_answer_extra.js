ae76c94924e587537479396b8fcb3b9f8c8ca822
Ilya Novikov <ilya.no@developmentmill.com>
2013-05-10 19:47:27 +0700

    course certificate section and css fixes

--- sites/all/modules/custom/best_answer_extra/best_answer_extra.js
+++ sites/all/modules/custom/best_answer_extra/best_answer_extra.js
@@ -16,7 +16,7 @@
       // bind event from flag module
       $(document).bind('flagGlobalBeforeLinkUpdate', function(event, data) {
         if (data.contentId > 0 && data.flagName === 'best_answer' && data.flagStatus === 'flagged') {
-          $('a.unflag-action').each(function() {
+          $('.flag-best-answer a.unflag-action').each(function() {
             $(this).removeClass('unflag-action').addClass('flag-action');
             this.href = this.href.replace('flag/unflag', 'flag/flag');
           });
--- sites/all/modules/custom/course_outline/course_outline.js
+++ sites/all/modules/custom/course_outline/course_outline.js
@@ -6,10 +6,20 @@
 (function($) {
   Drupal.behaviors.course_outline_accordion = {
     attach: function(context, settings) {
-     
       $('.js-outline-unit').unbind('click').bind('click', function() {
         $(this).next('ul').slideToggle().css('display', 'inline-block');
         $(this).toggleClass('active');
+      });
+      //disable certificate download link
+      $('.certificate-disabled a').click(function(e) {
+        e.preventDefault();
+      });
+      // define certificate popup position and show it
+      $('.outline-certificate-text').hover(function() {
+        var height = $('.outline-certificate-desc-wrapper').height() / 2;
+        var left = $(this).width() * 2;
+        $('.outline-certificate-desc-triangle').css('top', height - 5).css('left', -10);
+        $('.outline-certificate-desc-wrapper').css('top', 10 - height).css('left', left).fadeToggle('slow');
       });
       // open section with active page
       $('.block-course-outline .menu > ul').css('display', 'none');
