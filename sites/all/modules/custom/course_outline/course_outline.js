509d92a723a6ca67ca9181d436c61dea14a23308
Bulat <byapparov@gmail.com>
2013-05-10 17:20:46 +0100

    Certificate block refactoring.

--- sites/all/modules/custom/course_outline/course_outline.js
+++ sites/all/modules/custom/course_outline/course_outline.js
@@ -10,10 +10,19 @@
         $(this).next('ul').slideToggle().css('display', 'inline-block');
         $(this).toggleClass('active');
       });
-      //disable certificate download link
-      $('.certificate-disabled a').click(function(e) {
-        e.preventDefault();
-      });
+      // open section with active page
+      $('.block-course-outline .menu > ul').css('display', 'none');
+      $('.block-course-outline .active').closest('ul').css('display', 'inline-block');
+
+      $('.book-navigation a.page-up').html($('.block-course-outline a.active').html());
+      $('.book-navigation a.page-up').attr('href', '#');
+    }
+  };
+})(jQuery);
+
+(function($) {
+   Drupal.behaviors.course_outline_certificate = {
+    attach: function(context, settings) {
       // define certificate popup position and show it
       $('.outline-certificate-text').hover(function() {
         var height = $('.outline-certificate-desc-wrapper').height() / 2;
@@ -21,12 +30,6 @@
         $('.outline-certificate-desc-triangle').css('top', height - 5).css('left', -10);
         $('.outline-certificate-desc-wrapper').css('top', 10 - height).css('left', left).fadeToggle('slow');
       });
-      // open section with active page
-      $('.block-course-outline .menu > ul').css('display', 'none');
-      $('.block-course-outline .active').closest('ul').css('display', 'inline-block');
-
-      $('.book-navigation a.page-up').html($('.block-course-outline a.active').html());
-      $('.book-navigation a.page-up').attr('href', '#');
     }
   };
 })(jQuery);
