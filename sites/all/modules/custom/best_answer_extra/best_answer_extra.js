5317edae6a13f5f5045b4e3a761dbb1993684b4f
Bulat <byapparov@gmail.com>
2013-05-11 19:15:24 +0100

    Removed dependency of best_answer on best answers module. Made it closer to approach used in answer 7.x-4.x.

--- sites/all/modules/custom/best_answer_extra/best_answer_extra.js
+++ sites/all/modules/custom/best_answer_extra/best_answer_extra.js
@@ -5,14 +5,6 @@
 (function($) {
   Drupal.behaviors.best_answer_extra = {
     attach: function(context, settings) {
-      var best = settings.best_answer;
-      if (best && best.nid.length > 0) {
-        $('.views-field-ops').each(function() {
-          if ($(this).find('.field-content').empty() && $(this).find('#' + best.nid)) {
-            $('#' + best.nid).addClass('best-answer-others');
-          }
-        });
-      }
       // bind event from flag module
       $(document).bind('flagGlobalBeforeLinkUpdate', function(event, data) {
         if (data.contentId > 0 && data.flagName === 'best_answer' && data.flagStatus === 'flagged') {
