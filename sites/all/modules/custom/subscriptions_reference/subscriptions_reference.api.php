ccb96b4a451f08b592c4ec6570d791e13fae0c92
Bulat <byapparov@gmail.com>
2013-05-15 19:34:21 +0100

    Documentation review for subscriptions_reference.

--- sites/all/modules/custom/subscriptions_reference/subscriptions_reference.api.php
+++ sites/all/modules/custom/subscriptions_reference/subscriptions_reference.api.php
@@ -6,14 +6,28 @@
  */
 
 /**
- * Gets attributes of the reference field
+ * Gets attributes of the reference field that controls notifications.
+ * 
+ * Function returns an array with the information about referncing fields where 
+ * each field is represented by an array with the following elements:
+ * - 'parent': Content type that is being referenced.
+ * - 'child': Content type that contains the referencing field.
+ * - 'field': The name of the referencing field.
+ * - 'ref_item': Element in the reference field that stores nid. 
+ *    For field based on References module it will be 'nid.
+ *    For fields based on Entity References it will be 'target_id'.
+ * 
  * @return array
+ *   Array as described above. 
+ *   Keys of array should be unique accross modules that implement this hook.
  */
 function hook_subscriptions_reference_info() {
    return array(
-    'reference_id' => array( // should unique accross modules that implement that hook
-      'parent' => 'parent_content_type',  // is being referenced
-      'child' => 'child_content_type',    // contains the field
-      'field' => 'my_reference_field'),   // name of the field
+     // 
+    'reference_id' => array( 
+      'parent' => 'parent_content_type',  
+      'child' => 'child_content_type',
+      'field' => 'my_reference_field',
+      'ref_item' => 'target_id'),  
   );
 }
