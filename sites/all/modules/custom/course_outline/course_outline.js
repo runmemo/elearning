8074959ba047c37aec133f7ca0ce831a635eba99
Bulat <byapparov@gmail.com>
2013-05-17 12:29:20 +0100

    Fixed minor styling issues on course outline block.

--- sites/all/modules/custom/course_outline/course_outline.js
+++ sites/all/modules/custom/course_outline/course_outline.js
@@ -7,15 +7,13 @@
   Drupal.behaviors.course_outline_accordion = {
     attach: function(context, settings) {
       $('.js-outline-unit').unbind('click').bind('click', function() {
-        $(this).next('ul').slideToggle().css('display', 'inline-block');
+        $(this).next('ul').toggle();
         $(this).toggleClass('active');
       });
       // open section with active page
       $('.block-course-outline .menu > ul').css('display', 'none');
-      $('.block-course-outline .active').closest('ul').css('display', 'inline-block');
-
-      $('.book-navigation a.page-up').html($('.block-course-outline a.active').html());
-      $('.book-navigation a.page-up').attr('href', '#');
+      $('.block-course-outline .active').closest('ul').toggle();
+      $('.block-course-outline .active').closest('ul').prev().toggleClass('active');
     }
   };
 })(jQuery);
