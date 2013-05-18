fb281683bba59f3e21e9782602babe2858fa42d9
Bulat <byapparov@gmail.com>
2013-05-18 01:26:33 +0100

    Refactoring of js and css classes for grades tooltip.

--- sites/all/modules/custom/course_outline/course-outline-certifcate.tpl.php
+++ sites/all/modules/custom/course_outline/course-outline-certifcate.tpl.php
@@ -19,26 +19,26 @@
       <div class="outline-certificate-star"></div>
       <span class="outline-certificate-text"><?php print $points ?></span>
       <?php if (isset($grade_ranges) && !empty($grade_ranges)) : ?>
-        <div class="outline-certificate-desc-wrapper">
-          <div class="outline-certificate-desc-triangle"></div>
-            <div class="outline-certificate-description">
-             <div class="header"><?php print t('Compliance scores and marks for the certificate'); ?></div>
-             <ul class="grades-list">
-               <?php 
-                    $count = count($grade_ranges);
-                    $i = 0;
-                    foreach ($grade_ranges as $range => $name): 
-               ?>
-                <li <?php print $i ? '' : 'class="first"'; ?><?php print $i + 1 == $count ? 'class="last"' : '' ?>>
-                 <span class="points-range"><?php print t('!range points', array('!range' => $range)); ?></span>
-                 <span class="points-desc"> - <?php print $name ?></span>
+        <div class="tooltip-wrapper">
+          <div class="tooltip-triangle"></div>
+          <div class="tooltip-header"><?php print t('Compliance scores and marks for the certificate'); ?></div>
+          <div class="tooltip-content">
+            <ul class="course-outline-grades-list">
+              <?php
+              $count = count($grade_ranges);
+              $i = 0;
+              foreach ($grade_ranges as $range => $name):
+                ?>
+                <li <?php print $i ? '' : 'class="first"'; ?><?php print $i + 1 == $count ? 'class="last"' : ''  ?>>
+                  <span class="grade-points-range"><?php print t('!range points', array('!range' => $range)); ?></span>
+                  <span class="grade-points-desc"> - <?php print $name ?></span>
                 </li>
-               <?php 
-                    $i++;
-                  endforeach;
-               ?>
-              </ul>
-            </div>
+                <?php
+                $i++;
+              endforeach;
+              ?>
+            </ul>
+          </div>
         </div>
       <?php endif; ?>
     </div>
--- sites/all/modules/custom/course_outline/course_outline.js
+++ sites/all/modules/custom/course_outline/course_outline.js
@@ -24,15 +24,21 @@
       // define certificate popup position and show it
       $('.outline-certificate-points').hover(
         function(e) {
-          var height = $('.outline-certificate-desc-wrapper').height() / 2;
+          $tooltip = get_tooltip($(this));
+          var height = $tooltip.height() / 2;
           var left = e.pageX - $(this).offset().left + 20;
           left = Math.max(left, $(this).width() * 2 / 3);
-          $('.outline-certificate-desc-triangle').css('top', height - 5).css('left', -10);
-          $('.outline-certificate-desc-wrapper').css('top', 10 - height).css('left', left).fadeIn(200);
+          $tooltip.find('.tooltip-triangle').css('top', height - 5).css('left', -10);
+          $tooltip.css('top', 10 - height).css('left', left).fadeIn(200);
         },
         function(e) {
-           $('.outline-certificate-desc-wrapper').stop(true, true).fadeOut();
+          $tooltip = get_tooltip($(this));
+          $tooltip.stop(true, true).fadeOut();
         });
+      
+      function get_tooltip($element) {
+        return $element.find('.tooltip-wrapper');
+      }
     }
   };
 })(jQuery);
