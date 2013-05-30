4b64722acf993ba4a1eb65608ed05b1996c10d28
Barinder Singh <sainibarinder@gmail.com>
2013-05-30 21:17:55 +0530

    Revert "Answer comment link visibility using js"
    This reverts commit 8775aca861d8a95cb30e055076b3e1bf222e0909.

--- sites/all/modules/custom/answers_extra/answers_extra.js
+++ sites/all/modules/custom/answers_extra/answers_extra.js
@@ -6,22 +6,13 @@
   Drupal.behaviors.answers_extra = {
     attach: function(context, settings) {
 
-      // Hover tooltip for answer edit.
+      // Hover tooltip on OQ2 edit form.
       $(".ae-answer-form-edit")
         .mouseenter(function() {
           $(this).next(".description").show();
         })
         .mouseleave(function() {
           $(this).next(".description").hide();
-        });
-
-        $.each(Drupal.settings.answersExtra, function(index, value) {
-          // get nid from index.
-          var nid = index.substring(14);
-          // if user dont have access to comment link remove link.
-          if(value == false) {
-            $("#comment-form-show-"+nid).remove();
-          }
         });
 
     }
