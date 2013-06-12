93df322c02faeb97e1a13ef24e702478931834b5
Bulat <byapparov@gmail.com>
2013-06-12 10:55:26 +0100

    Changed drush command.

--- jenkins/tests_run.sh
+++ jenkins/tests_run.sh
@@ -18,12 +18,11 @@ chmod 777 /tmp/tests/
 
 cd /var/www/html/coursehub
 
-alias drush_test="drush -l http://test.coursehub.ru"
-drush_test status
-drush_test updatedb --yes
-drush_test cache-clear all
+drush status
+drush updatedb --yes
+drush cache-clear all
 for testname in `cat ./jenkins/tests_list`; do
-drush_test test-run --xml=/tmp/tests/ ${testname}
+drush -l http://test.coursehub.ru test-run --xml=/tmp/tests/ ${testname}
   if [ $? -ne 0 ]; then
     echo "Failed to run test ${testname}"
     exit 1
