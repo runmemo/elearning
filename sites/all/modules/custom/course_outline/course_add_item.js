983f316eed62f5136aae12f100ff66eda42f8636
Barinder Singh <sainibarinder@gmail.com>
2013-07-24 13:50:51 +0530

    JS namespace fixed

--- sites/all/modules/custom/course_outline/course_add_item.js
+++ sites/all/modules/custom/course_outline/course_add_item.js
@@ -3,10 +3,10 @@
   Drupal.behaviors.course_outline_item_add = {
     attach: function(context, settings) {
       $('#course-add-item-lesson').ajaxStart(function(){  
-        $(".form-submit").attr("disabled", "disabled");
+        $("#course-add-item-lesson .form-submit").attr("disabled", "disabled");
       });
       $('#course-add-item-lesson').ajaxStop(function(){  
-        $(".form-submit").removeAttr("disabled");
+        $("#course-add-item-lesson .form-submit").removeAttr("disabled");
       });
     }
   };
