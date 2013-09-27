9e46451df4325eca03a7b212d5aeafb13da2d2d7
Bulat <byapparov@gmail.com>
2013-09-27 21:22:43 +0100

    Added shopfront module for promotion of the product.

---
+++ sites/all/themes/product/templates/node--webform.tpl.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @file node-course.tpl.php
+ * Template for course nodes.
+ *
+ * Available variables:
+ * - $provider: Array with Provider's logo image, name and extra fields.
+ * - $teacher: Array representing fields "Name", "Surame" and user picture
+ *             of the user, who is the course teacher
+ * - $form_class_participate: form for participating in the course
+ * - $students_view: embedded view with students, which are already participats
+ * - $content: node content
+ * 
+ * All variables available in node.tpl.php
+ */
+?>
+<div class ='we are here'> </div>
+<article<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <div class='webform grid-10 alpha'>
+       <?php print drupal_render($content['body']); ?>
+      <?php print drupal_render($content['webform']); ?>
+    </div>
+    <div class='grid-6 webform-sidebar-second omega'>
+      <div class='webform-action-call'>
+        <?php print drupal_render($content['field_call_invitation']); ?>
+        <?php print drupal_render($content['field_image']); ?>
+        
+        <?php print drupal_render($content['field_phone_number']); ?>
+      </div>
+      <div class='webform-moto'>
+        <?php print drupal_render($content['field_moto']); ?>
+      </div>
+    </div>
+  
+  </div>
+</article>
